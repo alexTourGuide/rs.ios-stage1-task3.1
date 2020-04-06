d08a4ed7d2abd25387b8e24e8bdf2c6553cb2df9
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-04-06 18:24:45 +0300

    feat: implement 2nd task

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,33 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+// use double to apply n <= 1030, otherwise - calculated at n <= 62
+    double mBinCoef = array[0].intValue;
+    double n = array[1].intValue;
+    
+    if (n == mBinCoef) {
+        return @1;
+    }
+    
+    for (int x = 1; x < n; x++) {
+        double nFact = [Combinator factorial:n];
+        double xDiffNxFact = [Combinator factorial:(n - x)] * [Combinator factorial:x];
+        double k = nFact / xDiffNxFact;
+        if (k >= mBinCoef) {
+           return [NSNumber numberWithInt:x];
+        }
+    }
+    return nil;
 }
+
+// factorialOfN calculation
++ (double)factorial:(double)n {
+    double fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
 @end
+
